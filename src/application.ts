c43ce0870a4165f0a8a168030ad00ba3895e1fb1
robocode2 <rabie.abbas@code.berlin>
2023-07-18 18:45:44 +0200

    bind BaseUserRepo explicitly

---
+++ src/application.ts
@@ -0,0 +1,58 @@
+import {BootMixin} from '@loopback/boot';
+import {ApplicationConfig} from '@loopback/core';
+import {
+  RestExplorerBindings,
+  RestExplorerComponent,
+} from '@loopback/rest-explorer';
+import {RepositoryMixin} from '@loopback/repository';
+import {RestApplication} from '@loopback/rest';
+import {ServiceMixin} from '@loopback/service-proxy';
+import path from 'path';
+import {MySequence} from './sequence';
+import { BaseUserRepository } from './repositories';
+
+export {ApplicationConfig};
+
+export class LibraryApplication extends BootMixin(
+  ServiceMixin(RepositoryMixin(RestApplication)),
+) {
+  constructor(options: ApplicationConfig = {}) {
+    super(options);
+
+    this.repository(BaseUserRepository);
+    
+    // Set up the custom sequence
+    this.sequence(MySequence);
+
+    // Set up default home page
+    this.static('/', path.join(__dirname, '../public'));
+
+    // Customize @loopback/rest-explorer configuration here
+    this.configure(RestExplorerBindings.COMPONENT).to({
+      path: '/explorer',
+      repositories: {
+        glob: '/repositories/*Repository.js'
+      },
+    });
+    this.component(RestExplorerComponent);
+
+    this.projectRoot = __dirname;
+    // Customize @loopback/boot Booter Conventions here
+    this.bootOptions = {
+      controllers: {
+        // Customize ControllerBooter Conventions here
+        dirs: ['controllers'],
+        extensions: ['.controller.js'],
+        nested: true,
+      },
+      datasources: {
+        glob: '/datasources/**/*.db.js'
+      },
+
+      services: {
+        // includes context services, use cases and repositories
+        glob: '/{bffs,contexts,shared}/**/*{ervice,UseCase,epository,Delegator}.js'
+      },
+    };
+  }
+}
